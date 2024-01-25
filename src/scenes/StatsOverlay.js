266df7460a71ea6779d4e5201145cf06768c15fa
Mohammad Taquie <alitaquie51@gmail.com>
2024-01-24 22:37:10 -0800

    first commit

---
+++ src/scenes/StatsOverlay.js
@@ -0,0 +1,12 @@
+class StatsOverlay extends Phaser.Scene{
+    constructor(){
+        super('statsoverlayScene')
+        console.log('Statsoverlay: constructor')
+    }
+    init(){
+        console.log('StatsOverlay: init')
+    }
+    update(){
+        console.log('StasOverlay: update')
+    }
+}
